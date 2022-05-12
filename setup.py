4a60fc8a1cecbc611599fbcc4aaee99fc04d0383
Steve de Peijper <steve.depeijper@gmail.com>
2022-05-12 15:45:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='pyNetatmoHue',
-    version='0.1.2',
+    version='0.1.3',
     classifiers=[        
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2.7',
