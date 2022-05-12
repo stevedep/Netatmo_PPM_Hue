3bca029c26baca28b26949d67d34109a823eb805
Steve de Peijper <steve.depeijper@gmail.com>
2022-05-12 10:11:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 # python setup.py --dry-run --verbose install
 
-from distutils.core import setup
-
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 
 setup(
