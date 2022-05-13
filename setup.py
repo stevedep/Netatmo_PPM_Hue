b71dff6068029b0f7fdc302f097c441f0c0a2ff4
Christian Clauss <cclauss@me.com>
2022-05-13 13:22:20 +0200

    Add more Python type hints

--- setup.py
+++ setup.py
@@ -14,8 +14,10 @@ setup(
     version="0.1.3",
     classifiers=[
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     author="Steve de Peijper",
     author_email="steve.depeijper@gmail.com",
